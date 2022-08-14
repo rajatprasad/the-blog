7e5c398616e559116f5ed75f180c55527b9d938a
rajat prasad <rajatprasad@rajats-MacBook-Air.local>
2022-08-14 20:24:36 +0530

    make it better

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,19 +5,19 @@ const Post = ({ post }) => {
   const PF = "https://dev-theblog.herokuapp.com/images/";
   return (
     <div className="post">
-      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
+      {post?.photo && <img className="postImg" src={PF + post?.photo} alt="" />}
 
       <div className="postInfo">
         <div className="postCats">
-          {post.categories.map((c) => (
+          {post?.categories.map((c) => (
             <span className="postCat">{c.name}</span>
           ))}
         </div>
-        <p className="postDate">{new Date(post.createdAt).toDateString()}</p>
-        <Link className="link" to={`/post/${post._id}`}>
-          <p className="postTitle">{post.title}</p>
+        <p className="postDate">{new Date(post?.createdAt).toDateString()}</p>
+        <Link className="link" to={`/post/${post?._id}`}>
+          <p className="postTitle">{post?.title}</p>
         </Link>
-        <p className="postDesc">{post.desc}</p>
+        <p className="postDesc">{post?.desc}</p>
       </div>
     </div>
   );
